6a4970d141a28eaaf4d0c34eccbbddc9447e1191
Jerome Quelin <jquelin@gmail.com>
2022-01-17 16:13:47 +0100

    logger: no hard requirement on colorlog

--- dvg/logger.py
+++ dvg/logger.py
@@ -1,15 +1,32 @@
-
-import colorlog
 import logging
 
-handler = colorlog.StreamHandler()
-handler.setFormatter(
-    colorlog.ColoredFormatter(
-        '%(log_color)s%(asctime)s %(module)s.%(funcName)s:%(lineno)d %(levelname)s %(message)s',
-        datefmt='%H:%M:%S'
+logger_name = 'dvg'
+
+try:
+    import colorlog
+except:
+    # no colorlog available
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(module)s.%(funcName)s:%(lineno)d %(levelname)s %(message)s',
+            datefmt='%H:%M:%S'
+        )
     )
-)
-log = colorlog.getLogger('dvg')
+
+    log = logging.getLogger(logger_name)
+
+else:
+    # colorlog available
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            '%(log_color)s%(asctime)s %(module)s.%(funcName)s:%(lineno)d %(levelname)s %(message)s',
+            datefmt='%H:%M:%S'
+        )
+    )
+    log = colorlog.getLogger(logger_name)
+
+
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
-
