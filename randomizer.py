37a71832440b3c21abcda6357d97250c7a138ba5
Jerome Quelin <jquelin@gmail.com>
2024-09-13 10:53:51 +0000

    using main entry point for dev script

--- randomizer.py
+++ randomizer.py
@@ -1,26 +1,4 @@
 #!/usr/bin/env python
-
-import argparse
-import os
-import sys
-
-bindir = os.path.dirname(__file__)
-sys.path.append(bindir)
-
-parser = argparse.ArgumentParser(
-    prog='randomizer',
-    description='What the program does',
-    epilog='---')
-
-parser.add_argument('-c', '--console', action='store_true')
-args = parser.parse_args()
-
-if args.console:
-    from dvg_randomizer.ui.console import UI
-    ui = UI()
-    ui.cmdloop()
-
-else:
-    from dvg_randomizer.ui.graphical import GUI
-    ui = GUI()
-    ui.mainloop()
+from dvg_randomizer.__main__ import run
+if __name__ == '__main__':
+    run()
